534bc42a357a7daeaa44d1765c5e3d274278b896
Khayrul Islam <khayrulislam2525@gmail.com>
2022-01-02 09:22:34 +0600

    navbar problem solved

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../About/About';
 import Slider from '../Slider/Slider'
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
     return (
         <div>
+            <Navbar />
             <Slider></Slider>
             <About />
         </div>
