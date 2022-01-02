fdc513cc5768519bf4935fc86ecd49965890fd60
farzanapomy <farzanapomy56@gmail.com>
2022-01-02 09:48:05 +0600

    udpate about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>This is about page from farzana </h1>
+      <h1>This is about page from farzana pomy</h1>
     </div>
   );
 };
