5f6593789af16a5f6c8dcab43c7a655ff127ef3b
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-02 23:02:50 +0600

    done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,21 +2,15 @@ import React from 'react';
 import Slider from '../Slider/Slider';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-<<<<<<< HEAD
-import About from '../About/About'
-=======
+
 import About from '../About/About';
 import Review from '../Reviews/Reviews'
->>>>>>> 018027d8a089077783706a4f26229fe16d5a8cf4
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Slider></Slider>
-<<<<<<< HEAD
-=======
             <Review></Review>
->>>>>>> 018027d8a089077783706a4f26229fe16d5a8cf4
             <About></About>
             <Footer />
         </div>
