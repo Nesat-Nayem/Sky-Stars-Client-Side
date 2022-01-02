8399797cdb95a173b9cd899335255eaef8d25117
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-02 20:03:56 +0600

    add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,14 +3,12 @@ import Slider from '../Slider/Slider'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer';
 import About from '../About/About'
-import Review from '../Reviews/Reviews'
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Slider></Slider>
             <About></About>
-            <Review></Review>
             <Footer />
         </div>
 
