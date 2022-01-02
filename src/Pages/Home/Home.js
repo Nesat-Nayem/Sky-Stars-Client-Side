6c83c94b5cb4c12872ea8ab4ca0a1aa85bec762e
farzanapomy <farzanapomy56@gmail.com>
2022-01-02 17:23:21 +0600

    added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,16 +3,18 @@ import Slider from '../Slider/Slider'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer';
 import About from '../About/About'
+import Review from '../Reviews/Reviews'
 const Home = () => {
     return (
         <div>
             <Navbar />
             <Slider></Slider>
             <About></About>
+            <Review></Review>
             <Footer />
         </div>
 
-        
+
     );
 };
 
