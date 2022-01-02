24e9e843c47e4e343123ae3fc8c2b2a4b48a9c79
farzanapomy <farzanapomy56@gmail.com>
2022-01-02 19:26:53 +0600

    update review

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-// import 'swiper/css/effect-fade';
-import img from '../../images/client.jpg'
-
-import 'swiper/css';
-import './Reviews.css'
 import { Grid } from '@mui/material';
+import 'swiper/css/effect-fade';
 import Rating from 'react-rating';
+import img from '../../images/client.jpg'
+// import 'swiper/css';
+import './Reviews.css'
 
 
 const Reviews = () => {
@@ -33,71 +32,73 @@ const Reviews = () => {
             <h1>
                 Happy Client testimonials
             </h1>
-            <Grid container spacing={2} sx={{ lineHeight: "40px" }}>
-                <Grid item xs={12} md={4}>
 
-                    <CardMedia
-                        sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
-                        component="img"
-                        image={img}
-                        alt="Paella dish"
-                    />
-                    <Rating readonly
-                        initialRating={5}
-                        fullSymbol={element1}
-                        emptySymbol={element2}>
+           
+                    <Grid container spacing={2} sx={{ lineHeight: "40px" }}>
+                        <Grid item xs={12} md={4}>
 
-                    </Rating>
-                    <Typography>
-                        Jhon Doe
-                    </Typography>
-                    <Typography>
-                        Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={4} >
-                    <CardMedia
-                        sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
-                        component="img"
-                        height="50"
-                        image={img}
-                        alt="Paella dish"
-                    />
-                    <Rating readonly
-                        initialRating={5}
-                        fullSymbol={element1}
-                        emptySymbol={element2}>
+                            <CardMedia
+                                sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
+                                component="img"
+                                image={img}
+                                alt="Paella dish"
+                            />
+                            <Rating readonly
+                                initialRating={5}
+                                fullSymbol={element1}
+                                emptySymbol={element2}>
 
-                    </Rating>
-                    <Typography>
-                        Jhon Doe
-                    </Typography>
-                    <Typography>
-                        Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <CardMedia
-                        sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
-                        component="img"
-                        image={img}
-                        alt="Paella dish"
-                    />
-                    <Rating readonly
-                        initialRating={5}
-                        fullSymbol={element1}
-                        emptySymbol={element2}>
+                            </Rating>
+                            <Typography>
+                                Jhon Doe
+                            </Typography>
+                            <Typography>
+                                Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4} >
+                            <CardMedia
+                                sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
+                                component="img"
+                                height="50"
+                                image={img}
+                                alt="Paella dish"
+                            />
+                            <Rating readonly
+                                initialRating={5}
+                                fullSymbol={element1}
+                                emptySymbol={element2}>
 
-                    </Rating>
-                    <Typography>
-                        Jhon Doe
-                    </Typography>
-                    <Typography>
-                        Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
-                    </Typography>
-                </Grid>
-            </Grid>
+                            </Rating>
+                            <Typography>
+                                Jhon Doe
+                            </Typography>
+                            <Typography>
+                                Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <CardMedia
+                                sx={{ width: "100px", height: "100px", borderRadius: "70%" }}
+                                component="img"
+                                image={img}
+                                alt="Paella dish"
+                            />
+                            <Rating readonly
+                                initialRating={5}
+                                fullSymbol={element1}
+                                emptySymbol={element2}>
 
+                            </Rating>
+                            <Typography>
+                                Jhon Doe
+                            </Typography>
+                            <Typography>
+                                Customer reviews build something known as social proof, a phenomenon that states people are influenced by those around them.
+                            </Typography>
+                        </Grid>
+                      
+                    </Grid>
 
         </Container>
     );
